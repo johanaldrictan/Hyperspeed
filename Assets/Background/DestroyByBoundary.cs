9477b15079b1f0e4f1816708421f6d32dd65faef
Johan Tan <johanaldrictan@gmail.com>
2019-01-15 17:53:51 -0800

    Started logic for determining if player is off screen(loss condition)

---
+++ Assets/Background/DestroyByBoundary.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyByBoundary : MonoBehaviour {
+
+    private GameController gameController;
+
+    void Start()
+    {
+        GameObject gameControllerObject = GameObject.FindWithTag("GameController");
+        if (gameControllerObject != null)
+        {
+            gameController = gameControllerObject.GetComponent<GameController>();
+        }
+        if (gameController == null)
+        {
+            Debug.Log("Cannot find 'GameController' script");
+        }
+    }
+    void OnTriggerExit(Collider other)
+    {
+        Debug.Log("Trigger exit");
+        if (other.CompareTag("Player"))
+        {
+            gameController.GameOver();
+        }
+        Destroy(other.gameObject);
+    }
+}
---
+++ Assets/GameController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour {
+
+    public GameObject playerPrefab;
+
+    private bool gameOver = false;
+
+	// Use this for initialization
+	void Start () {
+        SpawnPlayer();
+	}
+	
+	void Update () {
+        if (gameOver)
+        {
+            Debug.Log("Lose");
+        }
+	}
+
+    void SpawnPlayer()
+    {
+        Instantiate(playerPrefab);
+    }
+
+    public void GameOver()
+    {
+        gameOver = true;
+    }
+
+
+}
